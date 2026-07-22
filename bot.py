e6702a2a0def2ac7fcc17a18e8c1a8c7b58c1258
Nu <Thanurin8@gmail.com>
2026-07-22 19:25:43 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,65 +13,53 @@ from database import init_database
 app = Flask(__name__)
 
 
-telegram_app = None
-
-
 @app.route("/")
 def home():
-    return "🛡 CyberScan Bot is running!"
+    return "CyberScan Bot is running!"
 
 
 
 async def start_bot():
 
-    global telegram_app
-
     init_database()
 
-    telegram_app = Application.builder().token(
-        BOT_TOKEN
-    ).build()
+    bot = (
+        Application
+        .builder()
+        .token(BOT_TOKEN)
+        .build()
+    )
+
+    setup_handlers(bot)
+
+    print("🛡 CyberScan Bot Started...")
+
+    await bot.initialize()
+    await bot.start()
+
+    await bot.updater.start_polling()
 
 
-    setup_handlers(telegram_app)
 
 
-    await telegram_app.initialize()
+def run_async_bot():
 
-    await telegram_app.start()
-
-    await telegram_app.updater.start_polling()
-
-
-
-def run_telegram():
-
-    loop = asyncio.new_event_loop()
-
-    asyncio.set_event_loop(loop)
-
-    loop.run_until_complete(
+    asyncio.run(
         start_bot()
     )
 
 
-@app.before_request
-def start_once():
-
-    global telegram_app
-
-    if telegram_app is None:
-
-        import threading
-
-        threading.Thread(
-            target=run_telegram,
-            daemon=True
-        ).start()
-
-
 
 if __name__ == "__main__":
+
+    import threading
+
+
+    threading.Thread(
+        target=run_async_bot,
+        daemon=True
+    ).start()
+
 
     port = int(
         os.environ.get(
